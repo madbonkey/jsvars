8b9eecc2bae09826ceb9e1571ab47faf4a79966d
Nico Schneider <nico.tcap@gmail.com>
2014-03-07 00:04:48 +0100

    first test

--- jsvars.php
+++ jsvars.php
@@ -11,14 +11,22 @@ class Plugin_Jsvars extends Plugin
 		// reserve some attributes that wo don't want as js vars
 		$reserved = array('namespace', 'theme_path');
 
+		$html = array(
+			'wrap' => '<script>%s</script>',
+			'keyValuePair' => '%s = \'%s\'',
+		);
+
+		$vars = array();
 		foreach ($this->attributes() as $name => $value) {
 
 			// skip the reserved vars
 			if(in_array($name, $reserved)) continue;
 
+			$vars[] = sprintf($html['keyValuePair'], $name, $value);
+
 		}
 
-
+		return sprintf($html['wrap'], implode(",\n", $vars);
 
 	}
 
