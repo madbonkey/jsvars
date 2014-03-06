ba56c77b44dd9c2e25d1e2c6216951c25c6bec33
Nico Schneider <nico.tcap@gmail.com>
2014-03-07 00:16:55 +0100

    add namespacing

--- jsvars.php
+++ jsvars.php
@@ -11,22 +11,47 @@ class Plugin_Jsvars extends Plugin
 		// reserve some attributes that wo don't want as js vars
 		$reserved = array('namespace', 'theme_path');
 
+		// define our formats
 		$html = array(
-			'wrap' => '<script>%s</script>',
-			'keyValuePair' => 'var %s = \'%s\'',
+			'wrap' => "<script>%s</script>",
+			'keyValuePair' => 'var %s = \'%s\';',
+			'namespacedkeyValuePair' => '%s: \'%s\'',
+			'namespace' => 'var %s = {%s}',
 		);
 
 		$vars = array();
-		foreach ($this->attributes() as $name => $value) {
 
+		foreach ($this->attributes() as $name => $value) {
 			// skip the reserved vars
 			if(in_array($name, $reserved)) continue;
 
-			$vars[] = sprintf($html['keyValuePair'], $name, $value);
+			if($this->attribute('namespace')) {
+				// if we want to namespace, use the namespaced format
+				$vars[] = sprintf($html['namespacedkeyValuePair'], $name, $value);
+			} else {
+				// otherwise, just generate plain old key value pair vars
+				$vars[] = sprintf($html['keyValuePair'], $name, $value);
+			}
 
 		}
 
-		return sprintf($html['wrap'], implode(",\n", $vars));
+		if($this->attribute('namespace')) {
+			// return the namespaced object
+			return sprintf(
+						$html['wrap'],
+						sprintf(
+							$html['namespace'],
+							$this->attribute('namespace'),
+							implode(",\n", $vars)
+						)
+					);
+		} else {
+			// or just return concatenated vars
+			return sprintf(
+						$html['wrap'],
+						implode('', $vars)
+					);
+		}
 
 	}
 
